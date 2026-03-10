c50ae8ddf9a430840bcdfb3120457cbc3728fc7d
shubham-688 <agr.shubh.agr@gmail.com>
2026-03-10 18:39:26 -0400

    saving all grids for an eq in a csv

--- examples/example_csv.py
+++ examples/example_csv.py
@@ -3,12 +3,17 @@
 """
 Simple example of saving output to a CSV file.
 """
-
+taup_path="../../TauP-3.2.0-SNAPSHOT6/bin/taup"
 import csv
 import taup
 from scattererwhereartthou import SWAT, mapplot, sliceplot
 import sys,re,os
 import glob as glob
+import numpy as np
+# from obspy.taup import TauPyModel
+import taup
+import requests
+
 
 def extract_gridnumber(filename):
     match = re.search(r'gridnum(\d+)_', filename)
@@ -31,7 +36,7 @@ def extract_grid_nums(main_folder):
             gridnum_list.append(int(match.group(1)))
 
     return(gridnum_list)
-    
+
 def extract_datapackfile(grid_number,folder_datapack):
     for file_name in os.listdir(folder_datapack):
         if file_name.endswith('.txt'):
@@ -41,6 +46,9 @@ def extract_datapackfile(grid_number,folder_datapack):
                 return file_name
 ###
 def get_array_lat_long(eq_folder):
+    """
+    finds array deets (lat long) for an earthquake
+    """
     folder='/Users/keyser/Research/AK_all_stations/sac_files/'+eq_folder+'/'
     folder_datapack=folder+'data_pack/'
     gridnum_list=extract_grid_nums(folder)
@@ -81,8 +89,32 @@ def get_array_lat_long(eq_folder):
 
         deets_all.append(deets)
     return deets_all
+####
+def get_rp_for_leg(model, phase, src_depth_km, delta_deg_val,rcv_depth_km):
+    rps = model.get_ray_paths(
+        source_depth_in_km=float(src_depth_km),
+        distance_in_degree=float(delta_deg_val),
+        phase_list=[phase],receiver_depth_in_km=rcv_depth_km)
+    if not rps:
+        return None
+    return rps[0]
+
+def get_rp_using_taup(model, phase, evt,src_depth,sta):
+    with taup.TauPServer(taup_path=taup_path) as taupserver:
+    # query params correspond to the tools, one of:
+    # time, pierce, path, curve, discon, distaz, find, phase, refltrans, table, velplot, wavefront
+        params = taup.TimeQuery()
+        params.phase(phase)
+        params.model(model)
+        params.event(*evt)
+        params.station(*sta)
+        params.sourcedepth([src_depth])
+        timeResult = params.calc(taupserver)
+
+    return timeResult
+
 # location of taup version 3 executable, not needed if already on PATH
-taup_path="../../TauP-3.2.0-SNAPSHOT6/bin/taup"
+
 
 # -4.33 143.16 70.00 230402_180411_PA_inc2_r2.5
 # la/lo/elv: 64.67 -155.88 362 TA (GSA) (grid 65, -157)
@@ -92,11 +124,13 @@ taup_path="../../TauP-3.2.0-SNAPSHOT6/bin/taup"
 #PP slow= 8.23
 
 deets_all= get_array_lat_long('230402_180411_PA_inc2_r2.5')
-sys.exit()
+# sys.exit()
 model="iasp91"    # velocity model
-evt=(-4.33, 143.16)   # eq lat, lon
-eventdepth=70  # eq depth
-sta=(64.67, -155.88)  # station lat, lon
+# model_t = TauPyModel(model)
+
+evt=(deets_all[0]['Event'][0], deets_all[0]['Event'][1])   # eq lat, lon
+eventdepth=deets_all[0]['Event'][2]  # eq depth
+# sta=(64.67, -155.88)  # station lat, lon
 phase="P"   # reference phase
 max_dist_step=1.0 # max separation between path scatterers in degrees, default is 2 deg
 
@@ -105,11 +139,11 @@ max_dist_step=1.0 # max separation between path scatterers in degrees, default i
 # Note that the scatterer locations are quite sensitive to slowness, and so
 # a denser gridsearch over slowness may be needed if the min-max range is large
 # slownesses = [5.75+.05*p for p in range(-5, 6)]
-slownesses = [5.75+.25*p for p in range(10)]
+# slownesses = [5.75+.25*p for p in range(10)]
 # slownesses= list(range(5.6, 8, .25))
 # delay times relative to reference phase arrival, usually t_minus, t, t_plus
 # but can be more values for denser search results
-delaytimes = list(range(50, 171, 5))
+# delaytimes = list(range(50, 171, 5))
 # delaytimes=[50,55]
 bazoffset=0
 bazdelta=10
@@ -117,7 +151,7 @@ sta_scat_revphase="p,P,Ped,pP,PP"
 evt_scat_phase="p,P,Ped,pP,PP"
 
 
-with open("swat_230402_180411.csv", "w", newline='') as outcsv:
+with open("swat_230402_180411_all_grids.csv", "w", newline='') as outcsv:
     csvwriter = csv.writer(outcsv)
     csvwriter.writerow(["scatlat", "scatlon", "scatdepth",
                         "scatdistdeg", "scatbaz", "sta_scat_p", "scat_time",
@@ -125,39 +159,51 @@ with open("swat_230402_180411.csv", "w", newline='') as outcsv:
                         "evtlat", "evtlon", "evtdepth",
                         "stalat", "stalon",'baz_GCP'
                         ])
-    with taup.TauPServer( taup_path=taup_path) as taupserver:
-        print("starting...")
-        # reference phase
-        params = taup.PathQuery()
-        params.model(model)
-        params.event(*evt)
-        params.sourcedepth(eventdepth)
-        params.station(*sta)
-        params.phase(phase)
-        timeResult = params.calc(taupserver)
+    for grid in deets_all:
+        sta=(grid['ArrCen'][0],grid['ArrCen'][1])
+        dist=grid['Dist']
+        sP= get_rp_using_taup(model, "sP", evt, eventdepth,sta)
+        PP= get_rp_using_taup(model, "PP", evt, eventdepth,sta)
+        #### slowness and time between sP and PP.
+        slownesses = np.arange(sP.arrivals[0].rayparam+.5, PP.arrivals[0].rayparam-.5, 0.25)
+        delaytimes = np.arange(sP.arrivals[0].time+30, PP.arrivals[0].time-10, 5)
+        # list(range(50, 171, 5))
 
-        swatList = []
-        swat = SWAT(taupserver, eventdepth, model=model,
-            sta_scat_revphase=sta_scat_revphase,
-            evt_scat_phase=evt_scat_phase)
-        swat.event(*evt)
-        swat.station(*sta)
-        swat.dist_step = max_dist_step
+        # sys.exit()
+        with taup.TauPServer( taup_path=taup_path) as taupserver:
+            print("starting...")
+            # reference phase
+            params = taup.PathQuery()
+            params.model(model)
+            params.event(*evt)
+            params.sourcedepth(eventdepth)
+            params.station(*sta)
+            params.phase(phase)
+            timeResult = params.calc(taupserver)
 
-        for a in timeResult.arrivals:
-            print(f"Arrival: {a}")
-            traveltimes = [a.time+delay for delay in delaytimes]
-            print(f"slow: {slownesses}  delay: {delaytimes} traveltimes: {traveltimes}")
-            ans = swat.find_via_path(slownesses, traveltimes, bazoffset=bazoffset, bazdelta=bazdelta)
-            swatList.append(ans)
-            for sc in ans.scatterers:
-                csvwriter.writerow([format(sc.scat.lat, "0.3f"), format(sc.scat.lon, "0.3f"),
-                                    format(sc.scat.depth, "0.3f"),
-                                    format(sc.scat.distdeg, "0.2f"),
-                                    format(sc.scat_baz, "0.3f"),
-                                    format(sc.sta_scat_rayparam, "0.2f"),
-                                    format(sc.scat.time+sc.evt_scat.time, "0.2f"),
-                                    sc.sta_scat_phase,sc.evt_scat.phase,
-                                    ans.evtlat, ans.evtlon, ans.evtdepth,
-                                    ans.stalat, ans.stalon,format(ans.esbaz, "0.3f")
-                                    ])
+            swatList = []
+            swat = SWAT(taupserver, eventdepth, model=model,
+                sta_scat_revphase=sta_scat_revphase,
+                evt_scat_phase=evt_scat_phase)
+            swat.event(*evt)
+            swat.station(*sta)
+            swat.dist_step = max_dist_step
+
+            for a in timeResult.arrivals:
+                print(f"Arrival: {a}")
+                # traveltimes = [a.time+delay for delay in delaytimes] # used when using delay..
+                traveltimes = delaytimes # used for absolute
+                print(f"slow: {slownesses}  delay: {delaytimes} traveltimes: {traveltimes}")
+                ans = swat.find_via_path(slownesses, traveltimes, bazoffset=bazoffset, bazdelta=bazdelta)
+                swatList.append(ans)
+                for sc in ans.scatterers:
+                    csvwriter.writerow([format(sc.scat.lat, "0.3f"), format(sc.scat.lon, "0.3f"),
+                                        format(sc.scat.depth, "0.3f"),
+                                        format(sc.scat.distdeg, "0.2f"),
+                                        format(sc.scat_baz, "0.3f"),
+                                        format(sc.sta_scat_rayparam, "0.2f"),
+                                        format(sc.scat.time+sc.evt_scat.time, "0.2f"),
+                                        sc.sta_scat_phase,sc.evt_scat.phase,
+                                        ans.evtlat, ans.evtlon, ans.evtdepth,
+                                        ans.stalat, ans.stalon,format(ans.esbaz, "0.3f")
+                                        ])
